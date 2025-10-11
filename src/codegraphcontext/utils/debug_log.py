595fb759d73d5aebfa23120d16cd2100ff0312d3
Sreeja Khan <voicenote8lifeline@gmail.com>
2025-10-11 19:18:07 +0530

    feat:resolved bool_variable issue.

--- src/codegraphcontext/utils/debug_log.py
+++ src/codegraphcontext/utils/debug_log.py
@@ -1,8 +1,13 @@
 import os
 from datetime import datetime
 
+# Toggle this to True to enable debug logging
+debug_mode = False  # Set to True for dev/test, False for production
+
 def debug_log(message):
-    """Write debug message to a file"""
+    """Write debug message to a file if debug_mode is enabled"""
+    if not debug_mode:
+        return
     debug_file = os.path.expanduser("~/mcp_debug.log")
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open(debug_file, "a") as f:
